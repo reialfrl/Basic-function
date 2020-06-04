ec2320790faa8f7fad44a075b252d4a0381c9ebe
Reinaldo Alfonso Ramirez Lobo <65871067+reialfrl@users.noreply.github.com>
2020-06-04 15:56:51 -0500

    Update cifrado_cesar.py

--- cifrado_cesar.py
+++ cifrado_cesar.py
@@ -16,11 +16,9 @@ def cifrado(sentencia, llave=10):
 
     return cifrado
                        
-
 a = cifrado('Hola Como estaz', 23)
 print(a)
         
-
 def descifrado(sentencia, llave=10):
     sentencia = sentencia.lower()
     alfabeto = string.ascii_lowercase
@@ -36,7 +34,6 @@ def descifrado(sentencia, llave=10):
             cifrado += letra
 
     return cifrado
-
 
 a = descifrado('elix zljl bpqxw', 23)
 print(a)
