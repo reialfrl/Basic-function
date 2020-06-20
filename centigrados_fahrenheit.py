3f40881bb589f8216788f50133acecc24acc2106
Reinaldo Alfonso Ramirez Lobo <65871067+reialfrl@users.noreply.github.com>
2020-06-20 14:50:30 -0500

    Update centigrados_fahrenheit.py

--- centigrados_fahrenheit.py
+++ centigrados_fahrenheit.py
@@ -1,14 +1,11 @@
 if __name__ == "__main__":
     #Función para convertir los grados centigrados ingresados por el usuario a grados fahrenheit
-    def grados_fahrenheit(centigrados):
-        if centigrados <= 0:
-            print('El valor ingresado debe ser mayor a 0 o un numero positivo')
-        return (centigrados * 9 / 5) + 32
-
-    try:
-        centigrados = float(
-            input('Ingrese la cantidad de grados centigrados que desea convertir:\n'))
-        total = grados_fahrenheit(centigrados)
-        print(f'{centigrados} ºC son:\n{total} ºF')
-    except ValueError:
-        print('El dato que ingreso es incorrecto, solo se aceptan numeros positivos')
+    def grados_fahrenheit():
+        try:
+            centigrados = float(input('Ingrese la cantidad de grados centigrados que desea convertir: '))
+            if centigrados <= 0:
+                print('El valor ingresado debe ser mayor a 0 o un numero positivo')
+            else:
+                return print(f'{centigrados} ºC son: {(centigrados * 9 / 5) + 32} ºF')
+        except ValueError:
+            print('El dato que ingreso es incorrecto, solo se aceptan numeros positivos')tivos')
